dc6874792fbe91a95e3747c82f1ed43c49ab9365
hikaruna <me@hikaruna.net>
2018-05-29 01:17:48 +0900

    add validateProductIdentifiers

--- SyncedStoreKit/SyncedStoreKit.h
+++ SyncedStoreKit/SyncedStoreKit.h
@@ -1,11 +1,3 @@
-//
-//  SyncedStoreKit.h
-//  SyncedStoreKit
-//
-//  Created by 中川輝 on 2018/05/29.
-//  Copyright © 2018年 hikaruna. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for SyncedStoreKit.
