9938436995feb878c4f2eebc39215c9de28bd5d7
hikaruna <me@hikaruna.net>
2018-05-29 00:37:29 +0900

    create xcode project as cocoa touch framework

---
+++ SyncedStoreKit/SyncedStoreKit.h
@@ -0,0 +1,19 @@
+//
+//  SyncedStoreKit.h
+//  SyncedStoreKit
+//
+//  Created by 中川輝 on 2018/05/29.
+//  Copyright © 2018年 hikaruna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SyncedStoreKit.
+FOUNDATION_EXPORT double SyncedStoreKitVersionNumber;
+
+//! Project version string for SyncedStoreKit.
+FOUNDATION_EXPORT const unsigned char SyncedStoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyncedStoreKit/PublicHeader.h>
+
+
